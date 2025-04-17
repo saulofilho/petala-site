9c91c3af14abf3b9aed981d3be48a04ee5eecbbc
Saulo Filho <saulosilvaf@gmail.com>
2025-04-17 16:27:00 -0300

    fix mobile css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,13 @@
 export function Footer() {
   const footer = document.createElement('footer');
   footer.innerHTML = `
-      <div class="footer-wrapper">
-        <div class="infos">
-          <p>Avenida Rio Branco</p>
-          <p>Nº 404, Sala 307, Bloco 1 - Centro</p>
-          <p>Cuiabá - MT</p>
-        </div>
-        <div class="infos">
-          <p>lucilenemirandola@gmail.com</p>
-        </div>
+    <div class="footer-wrapper">
+      <div class="infos">
+        <p>Fale com a gente:</p>
+      </div>
+      <div class="infos">
+        <p>lucilenemirandola@gmail.com</p>
+      </div>
     </div>
     <div class="copyright">
       <p>
